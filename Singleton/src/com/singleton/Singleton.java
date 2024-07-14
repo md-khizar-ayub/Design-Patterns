d27c402071b3faf35f618263d9108928eb5be4b1
moham <moham@MSI>
2024-07-14 18:35:48 +0530

    making private constructor and synchronizing instance

--- Singleton/src/com/singleton/Singleton.java
+++ Singleton/src/com/singleton/Singleton.java
@@ -5,16 +5,18 @@ public class Singleton {
 	private static Singleton instance;
 	private String value;
 	
-	public Singleton(String value2) {
+	private Singleton(String value2) {
 		this.value = value2;
 	}
 
 	public static Singleton getInstance(String value) {
-		
-		if(instance==null) {
-			return new Singleton(value);
+		synchronized (Singleton.class) {
+			if(instance==null) {
+				return new Singleton(value);
+			}
+			return instance;
 		}
-		return instance;
+		
 	}
 	public String getValue() {
 		return this.value;
